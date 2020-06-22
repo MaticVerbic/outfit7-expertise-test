85bc6023be6fa28c0bc8d39a605f5beb8d3c1108
MaticVerbic <matic.verbicit@gmail.com>
2020-06-22 14:35:42 +0200

    Added comments, general bugfixes

--- server/endpoints/list.go
+++ server/endpoints/list.go
@@ -28,26 +28,29 @@ var required = []string{
 
 // List handles /list endpoint functionality.
 var List = func(w http.ResponseWriter, r *http.Request) {
+	// Fetch logger from logger middleware.
 	log, ok := r.Context().Value(config.LogKey).(*logrus.Entry)
 	if !ok {
 		log = logrus.NewEntry(logrus.New())
 		log.Error("failed to fetch logger")
 	}
 
+	// Authorize the client.
 	if !authorize(r.Context(), w, "any") {
 		log.WithField("user", r.Context().Value(config.UserKey)).Debug("authorized")
 		return
 	}
 
+	// Validate request
+	// Method check
 	if r.Method != http.MethodGet {
 		log.Error("invalid method")
 		writeResponse(w, 400, fmt.Sprintf("invalid method"), nil)
+		return
 	}
 
+	// Validate input
 	vals := r.URL.Query()
-	h := handler.GetInstance()
-	h.SetLogger(log)
-
 	for _, item := range required {
 		rec := vals[item]
 		if rec == nil || len(rec) > 1 || len(rec) == 0 || rec[0] == "" {
@@ -57,6 +60,16 @@ var List = func(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
+	// Check if redis is not empty.
+	if data, err := config.GetInstance().RedisClient.DBSize().Result(); data == 0 || err != nil {
+		log.Error("cache empty")
+		writeResponse(w, http.StatusInternalServerError, "internal system error", nil)
+		return
+	}
+
+	// Try to fetch desired country
+	h := handler.GetInstance()
+	h.SetLogger(log)
 	out, err := h.Get(vals["countryCode"][0])
 	if err != nil {
 		log.Error(errors.Wrapf(err, "failed to fetch list for country %q", vals["countryCode"][0]))
@@ -64,6 +77,11 @@ var List = func(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// If cache miss occurs, retry with a random country.
+	// Run both pre- and post- filter processes on random set.
+	// If any of the three arrays is empty, retry with a different random set.
+	// This ensures all 3 lists are returned non empty.
+	// If all checks fail return the error.
 	if out == nil {
 		log.WithField("countryCode", vals["countryCode"][0]).Warn("cache miss")
 		out, err = h.GetRandom()
@@ -108,8 +126,12 @@ var List = func(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Cache hit, postfilter
 	out = h.Postfilter(vals, out)
 
+	// Incase postfilter caused empty lists retry.
+	// If last check before exiting the retrying phase has yielded an error,
+	// then return an error to the user.
 	if testEmpty(out) {
 		for i := 0; i < config.GetInstance().RetryAttempts; i++ {
 			err = nil
@@ -134,12 +156,14 @@ var List = func(w http.ResponseWriter, r *http.Request) {
 	writeResponse(w, http.StatusOK, "", out)
 }
 
+// returns true/false depending on the size of AdNetwork arrays.
 func testEmpty(an *adnetwork.AdNetwork) bool {
 	return len(an.Banner) == 0 ||
 		len(an.Interstitial) == 0 ||
 		len(an.Video) == 0
 }
 
+// retry to fetch and process a random country, to achieve non-null lists.
 func retry(vals url.Values) (*adnetwork.AdNetwork, error) {
 	h := handler.GetInstance()
 
@@ -159,6 +183,7 @@ func retry(vals url.Values) (*adnetwork.AdNetwork, error) {
 	return out, nil
 }
 
+// helper to write response to users.
 func writeResponse(w http.ResponseWriter, status int, errStr string, out *adnetwork.AdNetwork) {
 	w.WriteHeader(status)
 
--- server/endpoints/update.go
+++ server/endpoints/update.go
@@ -14,27 +14,41 @@ import (
 
 // Update handles /list endpoint functionality.
 var Update = func(w http.ResponseWriter, r *http.Request) {
+	// Fetch logger from logger middleware.
 	log, ok := r.Context().Value(config.LogKey).(*logrus.Entry)
 	if !ok {
 		log = logrus.NewEntry(logrus.New())
 		log.Error("failed to fetch logger")
 	}
 
+	// Authorize the client.
 	if !authorize(r.Context(), w, "admin") {
 		log.WithField("user", r.Context().Value(config.UserKey)).Debug("authorized")
 		return
 	}
 
+	// Validate request
+	// Method check
 	if r.Method != http.MethodPost {
 		log.Error("invalid http method on update")
 		writeResponse(w, 400, fmt.Sprintf("invalid method"), nil)
 	}
 
+	// Validate body
 	if r.Body == nil {
 		log.Error("empty body on update")
 		writeResponse(w, 400, fmt.Sprintf("invalid empty request"), nil)
 	}
 
+	// Handle arguments
+	dropDB := false
+	if r.URL.Query()["wipe"] != nil &&
+		len(r.URL.Query()["wipe"]) == 1 &&
+		r.URL.Query()["wipe"][0] == "true" {
+		dropDB = true
+	}
+
+	// Parse body
 	in := &handler.LoadObject{}
 	b, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -51,6 +65,7 @@ var Update = func(w http.ResponseWriter, r *http.Request) {
 		writeResponse(w, 500, fmt.Sprintf("internal system error"), nil)
 	}
 
+	// Parse data and store
 	h := handler.GetInstance()
 	h.SetLogger(log)
 
@@ -60,7 +75,7 @@ var Update = func(w http.ResponseWriter, r *http.Request) {
 		writeResponse(w, 500, fmt.Sprintf("internal system error"), nil)
 	}
 
-	if err = h.Store(m, true); err != nil {
+	if err = h.Store(m, dropDB); err != nil {
 		writeResponse(w, 500, fmt.Sprintf("internal system error"), nil)
 	}
 
